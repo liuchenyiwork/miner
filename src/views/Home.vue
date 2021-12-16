3a765fbe67443b16e99c8bf5587c5224eacadabb
forthespada <forthespada@foxmail.com>
2021-12-16 23:53:41 +0800

    feat: init file

---
+++ src/views/Home.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <el-button> 测试一下  </el-button>
+    <el-row>
+      <el-col>
+        我是来测试以一下的
+      </el-col>
+    </el-row>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    HelloWorld
+  }
+}
+</script>
